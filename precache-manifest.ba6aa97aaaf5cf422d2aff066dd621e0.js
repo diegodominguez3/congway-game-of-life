79c426f37149566a4128d080eb48433aab0deba3
Diego Dominguez <diego.dominguez@gammapartners.com>
2020-09-13 16:58:40 -0600

    Updates

---
+++ precache-manifest.ba6aa97aaaf5cf422d2aff066dd621e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e428ca52afb13a8938500dbc116465f",
+    "url": "/conway-game-of-life/index.html"
+  },
+  {
+    "revision": "ebf4d3a0bf8ca265586c",
+    "url": "/conway-game-of-life/static/css/main.4b85e9f8.chunk.css"
+  },
+  {
+    "revision": "00ed1c64fe8be0232acc",
+    "url": "/conway-game-of-life/static/js/2.424fc1ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/conway-game-of-life/static/js/2.424fc1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf4d3a0bf8ca265586c",
+    "url": "/conway-game-of-life/static/js/main.ad735f4d.chunk.js"
+  },
+  {
+    "revision": "7e6fb7af2d037d24ec98",
+    "url": "/conway-game-of-life/static/js/runtime-main.8cbe773b.js"
+  }
+]);
